27e4717964ce806be60b0322c192d4685f91538c
Anatoly Kirillov <kirillovtolia@gmail.com>
2022-08-30 19:23:46 +0300

    Task_26

---
+++ Task_26/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе(456->3;78->2;89126->5)
+int CountNumbers(int number)
+{
+    int result = 0;
+    while(number > 0)
+    {
+        result = result + 1;
+        number = number / 10;
+    }
+    return result;
+}
+
+Console.WriteLine("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+Console.WriteLine($"В числе{num} - {CountNumbers(num)} цифр(ы).");
