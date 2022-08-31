2ca4ca9d32c8b6613264f0afe9840812fad5461d
Anatoly Kirillov <kirillovtolia@gmail.com>
2022-08-31 09:56:13 +0300

    add task_28

---
+++ Task_28/Program.cs
@@ -0,0 +1,14 @@
+﻿// напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N(4->24;5->120)
+int ProdNumbers(int number)
+{
+    int result = 1;
+    for(int i = 1; i <= number; i = i + 1)
+    {
+        result = result * i;
+    }
+    return result;
+}
+
+Console.WriteLine("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+Console.WriteLine($"Произведение чисел от 1 до {num} = {ProdNumbers(num)}");
